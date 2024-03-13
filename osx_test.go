03681abcc8e32481d9695762c2dffd83a974889d
Mirza Hilmi <exquisitemirza@student.ub.ac.id>
2024-03-13 11:25:49 +0700

    test: Benchmark workdir function

--- osx_test.go
+++ osx_test.go
@@ -14,3 +14,12 @@ func TestWorkdir(t *testing.T) {
 		t.Errorf("expected: %v\nactual: %s", "not empty", wd)
 	}
 }
+
+func BenchmarkWorkdir(b *testing.B) {
+	b.ReportAllocs()
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		_, _ = Workdir()
+	}
+}
